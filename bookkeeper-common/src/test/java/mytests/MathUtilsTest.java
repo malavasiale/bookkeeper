5a6e717f8e341694201042b90fea4c9ab0d14c1b
malavasiale <malavasialessio@gmail.com>
2020-06-11 14:31:06 +0000

    MathUtils tests

--- bookkeeper-common/src/test/java/mytests/MathUtilsTest.java
+++ bookkeeper-common/src/test/java/mytests/MathUtilsTest.java
@@ -12,12 +12,22 @@ import org.junit.Test;
 public class MathUtilsTest {
 
 	/*
-	 * Test for 
+	 * First example test for MathUtils
 	 * */
 	@Test
-	public void test() {
+	public void test1() {
 		int mod = MathUtils.signSafeMod(10, 2);
 		assertEquals(0, mod);
 	}
-
+	
+	@Test
+	public void test2() {
+		int mod = MathUtils.signSafeMod(-11, 2);
+		assertEquals(1, mod);
+	}
+	
+	@Test(expected = Exception.class)
+	public void test3() {
+		MathUtils.signSafeMod(20, 0);
+	}
 }
